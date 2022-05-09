eff0df79b5b6e78ce6dccf18d79a8f3bf36cb203
Machine <244191147@qq.com>
2022-05-09 19:58:09 +0800

    我滴任务完成啦！

--- MachineTools-Txt_to_pdf.py
+++ MachineTools-Txt_to_pdf.py
@@ -1,103 +1,118 @@
-# coding: utf-8
-
-# setting sts font utf-8
-from hashlib import sha1
-from reportlab.lib.pagesizes import A4
-from reportlab.platypus import PageBreak
-from reportlab.platypus.tableofcontents import TableOfContents
-from reportlab.platypus import BaseDocTemplate, Frame, PageTemplate, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfbase import pdfmetrics
-import sys
 import re
 
-pdfmetrics.registerFont(TTFont('STSONG', 'FS_GB2312.ttf'))  # register Font
-pdfmetrics.registerFont(TTFont('simhei', 'FS_GB2312.ttf'))  # register Font
-styles = getSampleStyleSheet()
-styles.add(ParagraphStyle(fontName='STSONG', name='STSONG',
-                          leading=20, fontSize=12, firstLineIndent=22, wordWrap='CJK'))
-styles.add(ParagraphStyle(fontName='simhei', name='simhei',
-                          leading=25, fontSize=14, wordWrap='CJK'))  # content Font
+from PyPDF2 import PdfFileReader as pdfr
+from PyPDF2 import PdfFileWriter as pdfw
+from reportlab.lib.styles import ParagraphStyle as PS
+from reportlab.lib.units import cm
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import PageBreak
+from reportlab.platypus.doctemplate import BaseDocTemplate, PageTemplate
+from reportlab.platypus.frames import Frame
+from reportlab.platypus.paragraph import Paragraph
+from reportlab.platypus.tableofcontents import TableOfContents
 
+pattern =  re.compile(r'(第[0-9]{1,3}章：.*)|(续[0-9]{1,2})')
+# 目录的正则表达式
+txt_path = "我家老婆来自一千年前.txt"
+# 源文件路径
+aim_pdf_path='我家老婆来自一千年前.pdf'
+# 目的PDF路径
+function_switch = {
+    "add_big_toc":0,
+    "build_toc":0,
+    # 生成页面目录
+    "build_left_bookmark":1
+    # 生成左边栏书签
+}
+# 三个功能的开关
+
+
+
+
+pdfmetrics.registerFont(TTFont('FS_GB2312', 'FS_GB2312.ttf'))  # register Font
+# 注册字体
 
 class MyDocTemplate(BaseDocTemplate):
+
     def __init__(self, filename, **kw):
         self.allowSplitting = 0
-        super().__init__(filename,**kw)
+        self._Machine_bookmark_list=[]
+        BaseDocTemplate.__init__(self, filename, **kw)
 
+        template = PageTemplate(
+            'normal', [Frame(2.5*cm, 2.5*cm, 15*cm, 25*cm, id='F1')])
+        self.addPageTemplates(template)
     def afterFlowable(self, flowable):
         "Registers TOC entries."
-        if isinstance(flowable, Paragraph):
+        if flowable.__class__.__name__ == 'Paragraph':
             text = flowable.getPlainText()
             style = flowable.style.name
             if style == 'Heading1':
-                level = 0
-            elif style == 'simhei':
-                level = 1
-            else:
-                return
-            E = [level, text, self.page]
-            #if we have a bookmark name append that to our notify data
-            bn = getattr(flowable,'_bookmarkName',None)
-            if bn is not None: E.append(bn)
-            self.notify('TOCEntry', tuple(E))
+                key = 'h2-%s' % self.seq.nextf('heading2')
+                self.canv.bookmarkPage(key)
+                self.notify('TOCEntry', (1, text, self.page, key))
+                self._Machine_bookmark_list.append({
+                    'title':text,
+                    'page_num':self.page
+                })
 
 
-# # this function makes our headings
-def doHeading(data, text, sty):
-    # create bookmarkname
-    bn = sha1(text.encode('utf-8')).hexdigest()
-    # modify paragraph text to include an anchor point with name bn
-    h = Paragraph(text + '<a name="%s"/>' % bn, sty)
-    # store the bookmark name on the flowable so afterFlowable can see this
-    h._bookmarkName = bn
-    data.append(h)
-    # Page Number
 
-
-def footer(canvas, doc):
-    page_num = canvas.getPageNumber()
-    canvas.saveState()
-    P = Paragraph("%d" % page_num,styles['Normal'])
-    w, h = P.wrap(doc.width, doc.bottomMargin)
-    P.drawOn(canvas, doc.leftMargin + w/2, h)
-    canvas.restoreState()
-
-    # load txt file
-
-
-def loadTxt(txt_path):
+def loading_txt(txt_path:str)->list:
     with open(txt_path, 'r') as f:
         txt_datas = f.readlines()
     return txt_datas
 
-
-def toPDF(txt_datas, pdf_path):
-    PDF = MyDocTemplate(pdf_path, pagesize=A4)
-    frame = Frame(PDF.leftMargin, PDF.bottomMargin, PDF.width, PDF.height,id='normal')
-    template = PageTemplate(frames=frame,onPage=footer)
-    PDF.addPageTemplates([template])
-
-    data = []
-    NUM = 0
-    # add txt
+def build_pdf_file(txt_datas:list,aim_pdf_path:str)->None:
+    if function_switch["add_big_toc"]:
+        h1 =   PS(name='Heading1', fontName="FS_GB2312",fontSize=14,leading=16)
+    else:
+        h1 =   PS(name='Heading1', fontName="FS_GB2312",leading=16)
+    body = PS(name='body', leading=12, fontName="FS_GB2312")
+    # Build story.
+    story = []
+    # For conciseness we use the same styles for headings and TOC entries
+    toc = TableOfContents()
+    toc.levelStyles = [h1]
+    if function_switch["build_toc"]==1:
+        story.append(toc)
+        story.append(PageBreak())
     for txt_data in txt_datas:
-        txt_data = txt_data.lstrip()  # remove left space
-        if len(txt_data) == 0:  # no text
-            continue
-        txt_data = txt_data
-        a = re.findall(r'(第[0-9]{1,3}章：.*)|(续[0-9]{1,2})',txt_data)
-    
-        if len(a)!=0:
-            doHeading(data, txt_data, styles['simhei'])
-        else:
-            data.append(Paragraph(txt_data, styles['STSONG']))
-        NUM = NUM + 1
-    PDF.multiBuild(data)
 
-if __name__ == "__main__":
-    txt_path = "我家老婆来自一千年前.txt"
-    pdf_path = "我家老婆来自一千年前.pdf"
-    txt_datas = loadTxt(txt_path)
-    toPDF(txt_datas, pdf_path)
+        if re.search(pattern, txt_data):
+            story.append(Paragraph(txt_data, h1))
+            # 标题页面使用h1段落样式
+        else:
+            story.append(Paragraph(txt_data,body ))
+            # 正文段落使用正常body样式
+    doc = MyDocTemplate(aim_pdf_path)
+    doc.multiBuild(story)
+
+    bookmark_list = doc._Machine_bookmark_list
+    if function_switch["build_left_bookmark"]:
+        build_left_bookmark_for_pdf(aim_pdf_path,bookmark_list)
+
+
+
+
+def build_left_bookmark_for_pdf(aim_pdf_path:str,bookmark_list:list)->None:
+    pdf_reader=pdfr(aim_pdf_path)
+    pdf_writer=pdfw()
+    pdfpage_num = pdf_reader.getNumPages()
+    for page_num in range(pdfpage_num):
+        the_page = pdf_reader.getPage(page_num)
+        pdf_writer.addPage(the_page)
+    for bookmark in bookmark_list:
+        pdf_writer.addBookmark(bookmark['title'],bookmark['page_num']-1)
+    with open(aim_pdf_path,'wb')as fp:
+        pdf_writer.write(fp)
+    pass
+
+
+
+
+if __name__ =="__main__":
+    txt_datas = loading_txt(txt_path)
+    build_pdf_file(txt_datas, aim_pdf_path)
+    pass
